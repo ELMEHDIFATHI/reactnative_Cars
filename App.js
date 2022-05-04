3c230add538e86f3f853d8d2dcec9072191eb11f
MEHDI-svg <mehdiblabo@gmail.com>
2022-05-04 17:51:45 +0000

    app basic  react nativ

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import CarsList from './containerList';
+import Header from './containerHeader/index';
+
+
+
+
 
 export default function App() {
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+          <Header />
+         <CarsList />
+      
     </View>
   );
 }
@@ -17,4 +25,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+ 
+
+
 });
